16edb540e52a02317f527227b952f222bc9c055a
undefined-user0 <168307000+undefined-user0@users.noreply.github.com>
2025-09-21 13:44:22 -0500

    base cambios, ejemplo uso de funcion draw

--- Code/Base.py
+++ Code/Base.py
@@ -1,10 +1,27 @@
 from pyray import *
 from raylib import *
 
-init_window(1920,1080, 'Base')
+windowXvalue = 1280
+windowYvalue = 720
+
+windowXtext = int(windowXvalue*0.1) 
+windowYtext = int(windowYvalue*0.1) 
+
+rectX = int(windowXvalue*0.1)
+rectY = int(windowYvalue*0.2)
+rectW = int(windowXvalue*0.8)
+rectH = int(windowYvalue*0.6)
+
+rect = [rectX,rectY,rectW,rectH]
+#rectRound = ()
+
+init_window(windowXvalue,windowYvalue, 'Base')
 
 while not window_should_close():
     begin_drawing()
+    clear_background(BROWN)
+    draw_text('Hello, World!', windowXtext, windowYtext, 20, WHITE)
+    draw_rectangle_rounded(rect, 0.2, 10, RED)
     end_drawing()
 
 close_window()
